2c2828d4e99b379942308be7242c240b29f64c45
thekarimovmurad <thekarimovmurad@gmail.com>
2024-03-05 16:56:11 +0100

    Update time system

---
+++ WindowsSutdownApp/Form1.Designer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace WindowsSutdownApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(255)))), ((int)(((byte)(240)))));
+            this.button1.Location = new System.Drawing.Point(50, 200);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 75);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Shut down";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(255)))), ((int)(((byte)(240)))));
+            this.button2.Location = new System.Drawing.Point(300, 200);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 75);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Restart";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(255)))), ((int)(((byte)(240)))));
+            this.button3.Location = new System.Drawing.Point(300, 300);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 75);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Sleep";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(108)))), ((int)(((byte)(255)))), ((int)(((byte)(240)))));
+            this.button4.Location = new System.Drawing.Point(50, 300);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 75);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Lock screen";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F);
+            this.textBox1.Location = new System.Drawing.Point(125, 75);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 53);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            //this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(28)))), ((int)(((byte)(48)))));
+            this.ClientSize = new System.Drawing.Size(500, 400);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private TextBox textBox1;
+    }
+}
+
---
+++ WindowsSutdownApp/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsSutdownApp
+{
+    public partial class Form1 : Form
+    {
+        public delegate void TimerDelegate();
+        public Form1()
+        {   
+            InitializeComponent();
+            textBox1.KeyPress += textBox1_KeyPress;
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StartTimerAndAction(Shutdown);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            StartTimerAndAction(Restart);
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            StartTimerAndAction(Sleep);
+        }
+        private void button4_Click(object sender, EventArgs e)
+        {
+            StartTimerAndAction(LockScreen);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void Shutdown()
+        {
+            Process.Start("shutdown", "/s /t 0");
+        }
+        private void Restart()
+        {
+            Process.Start("shutdown", "/r /t 0");
+        }
+        private void LockScreen()
+        {
+            Process.Start("rundll32.exe", "user32.dll,LockWorkStation");
+        }
+        private void Sleep()
+        {
+            SetSuspendState(false, true, true);
+        }
+        
+        public void StartTimerAndAction(TimerDelegate action)
+        {
+            int time;
+            if (textBox1.Text== null)
+                time = 0;
+            else
+                time= int.Parse(textBox1.Text);
+            Timer timer = new Timer();
+            timer.Interval= time*1000;
+            timer.Tick += (sender, e) =>
+            {
+                action.Invoke();
+                timer.Stop();
+            };
+            timer.Start();
+            textBox1.Text = "";
+        }
+
+        [System.Runtime.InteropServices.DllImport("powrprof.dll", SetLastError = true)]
+        [return: System.Runtime.InteropServices.MarshalAs(System.Runtime.InteropServices.UnmanagedType.Bool)]
+        public static extern bool SetSuspendState(bool hibernate, bool forceCritical, bool disableWakeEvent);
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+                e.Handled = true;
+        }
+
+        //private void howmanysecond(object sender, EventArgs e)
+        //{
+        //    textBox1
+        //}
+    }
+}
